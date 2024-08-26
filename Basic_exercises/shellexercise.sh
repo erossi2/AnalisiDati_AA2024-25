e04a13a4df57e5717be72f28c710b290d0ce441e
erossi2 <146084620+erossi2@users.noreply.github.com>
2024-08-26 17:42:33 +0200

    Create shellexercise.sh

---
+++ Basic_exercises/shellexercise.sh
@@ -0,0 +1,16 @@
+echo "Hello world! 
+the echo command repeats whatever you type next, then expands the variables 
+the path is"
+echo $PATH
+echo "an interesting command is 'more' which reads a text file from start to finish, e.g.
+more shellexercise.sh. Now I will run the command:"
+#
+#
+more shellexercise.sh
+#
+#comments are marked by the hash symbol (#). This comment appears because the script uses "more"
+echo "other useful commands: 
+history : gives the history of the commands used
+tail : Reads a file from the end instead of the beginning
+ps : Shows open processes on the machine
+"
