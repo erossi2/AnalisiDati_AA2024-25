e4650f6e0873d3777f449a230a67e13110bcd52f
erossi2 <146084620+erossi2@users.noreply.github.com>
2024-08-26 17:58:22 +0200

    Update shellexercise.sh

--- Basic_exercises/shellexercise.sh
+++ Basic_exercises/shellexercise.sh
@@ -1,11 +1,13 @@
-echo "Hello world! 
-the echo command repeats whatever you type next, then expands the variables 
-the path is"
+echo "The echo command repeats whatever you type next in quotation marks, e.g. echo Hello world!"
+echo "Hello world!"
+echo "--------------------------"
+echo  "It also give the value of a variables using the dollar ($), e.g.$PATH and the path is"
 echo $PATH
-echo "an interesting command is 'more' which reads a text file from start to finish, e.g.
-more shellexercise.sh. Now I will run the command:"
-
+echo "--------------------------"
+echo "--------------------------"
+echo "comments are marked by the hash symbol (#)"
 #comments are marked by the hash symbol (#). This comment appears because the script uses "more"
+echo "--------------------------"
 echo "other useful commands to learn: 
 history : gives the history of the commands used
 tail : Reads a file from the end instead of the beginning
@@ -19,5 +21,11 @@ touch: create file
 nano: update file"
 #
 #
+echo "--------------------------"
+echo "An interesting command is 'more' which reads a text file from start to finish, e.g.
+more shellexercise.sh. Now I will run the command: so you can see the output"
+echo "--------------------------"
 more shellexercise.sh
+echo "--------------------------"
+echo "More useful information can be found in https://www.coursera.org/articles/how-to-learn-linux"
 #
