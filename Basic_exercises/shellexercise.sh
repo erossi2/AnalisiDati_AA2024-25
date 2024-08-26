e4bda52c7658597dcff047dfb3570edd6bf95076
erossi2 <146084620+erossi2@users.noreply.github.com>
2024-08-26 17:49:03 +0200

    Update shellexercise.sh

--- Basic_exercises/shellexercise.sh
+++ Basic_exercises/shellexercise.sh
@@ -4,13 +4,20 @@ the path is"
 echo $PATH
 echo "an interesting command is 'more' which reads a text file from start to finish, e.g.
 more shellexercise.sh. Now I will run the command:"
+
+#comments are marked by the hash symbol (#). This comment appears because the script uses "more"
+echo "other useful commands to learn: 
+history : gives the history of the commands used
+tail : Reads a file from the end instead of the beginning
+ps : Shows open processes on the machine
+cd: change directory
+ls: list files
+rm: delete files
+pwd: get current directory
+mkdir: create directory
+touch: create file
+nano: update file"
 #
 #
 more shellexercise.sh
 #
-#comments are marked by the hash symbol (#). This comment appears because the script uses "more"
-echo "other useful commands: 
-history : gives the history of the commands used
-tail : Reads a file from the end instead of the beginning
-ps : Shows open processes on the machine
-"
