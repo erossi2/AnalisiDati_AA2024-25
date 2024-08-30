2f58062a1389c1df65b53fa4d8884ef8848926bb
erossi2 <146084620+erossi2@users.noreply.github.com>
2024-08-30 17:04:45 +0200

    Add files via upload

---
+++ Basic_exercises/Test_vector.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+
+print("That was easy, right? Now let's load some libraries! ")
+
+import vector
+
+p1 = vector.obj(px=1,py=0,pz=0,E=1)
+p2 = vector.obj(px=1.5,py=0,pz=0,E=200)
+
+print ("the sum of the momenta is ", p1+p2)
